afe042ce062fca59606574b1b47e7c8b562df3bc
margaritov <lubo@hush.com>
2018-09-20 11:21:19 +0300

    Lecture 1 exercises completed

---
+++ Lecture 1 Stack And Queues/StacksAndQueuesExercise/10_PoisonusPlants/PoisonusPlants.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10_PoisonusPlants
+{
+    class PoisonusPlants
+    {
+        static void Main(string[] args)
+        {
+            int countPlants = int.Parse(Console.ReadLine());
+            Queue<int> plants = new Queue<int>(Console.ReadLine().Split(" ").Select(int.Parse));
+            int countDays = 0;
+            while (true)
+            {
+                int dayBeginPlantsCount = plants.Count;
+                int leftPlantPesticide = plants.Peek();
+
+                for (int i = 0; i < dayBeginPlantsCount; i++)
+                {
+                    int currentPlantPesticide = plants.Dequeue();
+
+                    // if plant has less ot equal pesticide amount than left-side plant it survives
+                    if (currentPlantPesticide <= leftPlantPesticide)
+                    {
+                        plants.Enqueue(currentPlantPesticide);
+                    }
+
+                    leftPlantPesticide = currentPlantPesticide;
+                }
+
+                if (plants.Count == dayBeginPlantsCount)
+                { // no plants died - exit loop
+                    break;
+                }
+                countDays++;
+            }
+            Console.WriteLine(countDays.ToString());
+        }
+    }
+}
---
+++ Lecture 1 Stack And Queues/StacksAndQueuesExercise/9_SimpleTextEditor/SimpleTextEditor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _9_SimpleTextEditor
+{
+    class SimpleTextEditor
+    {
+        static void Main(string[] args)
+        {
+            int operationsCount = int.Parse(Console.ReadLine());
+            Stack<string> stack = new Stack<string>();
+            string text = "";
+            for (int i = 0; i < operationsCount; i++)
+            {
+                string[] command = Console.ReadLine().Split(" ");
+                switch (command[0])
+                {
+                    // appends to text
+                    case "1":
+                        stack.Push(text);
+                        text += command[1];
+                        break;
+                    // removes last n characters from text
+                    case "2":
+                        stack.Push(text);
+                        text = text.Substring(0, text.Length - int.Parse(command[1]));
+                        break;
+                    // returns n-th character from text
+                    case "3":
+                        Console.WriteLine(text[int.Parse(command[1]) - 1].ToString());
+                        break;
+                    // undo last append or removes command
+                    case "4":
+                        text = stack.Pop();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
